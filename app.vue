b41cfe66521ddbcb6a968828ba919dd59158cca6
Georgi Karchev <goshoo429@gmail.com>
2023-08-01 01:53:22 +0300

    Setup the pages

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <li><NuxtLink to="/about"> About </NuxtLink> </li>
+   <li><NuxtLink to="/portfolio"> Portfolio </NuxtLink> </li>
+    <NuxtPage/>
+  </div>
+</template>
+
+<script>
+    export default {
+       
+    }
+</script>
---
+++ pages/portfolio.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>PORTFOLIO</h1>
+  </div>
+</template>
