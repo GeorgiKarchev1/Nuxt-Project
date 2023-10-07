1291a9e6b86e3221a64eff32f6b9ff042f0e593f
Georgi Karchev <goshoo429@gmail.com>
2023-10-07 17:01:31 +0300

    Try to fix text position error

--- pages/index.vue
+++ pages/index.vue
@@ -34,15 +34,18 @@
       <div class="about-me-text">
         <h2>About Me</h2>
         <p>
-          Allow me to introduce myself, I am Georgi Karchev, a 17 years old from Plovdiv.
-          I am a passionate and self-taught developer with a strong aspiration to advance within the technology industry. Prior to my venture into the world of programming, I devoted my time to video editing, focusing on diverse content such as gaming, fitness, and automotive themes. Over the past two years, I have acquired valuable experience in this domain, collaborating with various clients to meet their specific requirements. If you are interested in collaborating with me, kindly click on the Contact button.
-        </p>
-      </div>
-      <div class="img-wrapper">
+          Let me introduce myself, I am Georgi Karchev, 17 years old, from Plovdiv.
+         I am a passionate and self-taught programmer with a strong drive to get ahead in the tech industry. 
+         Before entering the world of programming, I dedicated my time to video processing and video directing, but I figured that would remain as a side thing for now.
+         So if you want to work with me be it programming related, video editing or anything else you can contact me.
+</p>
+        <div class="img-wrapper">
         <img src="../assets/draham.png" class="lice-brato" alt="moeto lice"/>
       </div>
     </div>
   </div>
+      </div>
+      
     <div class="menu-wrapper" :class="{ 'menu-open': isMenuOpen }">
       <button v-if="isMenuOpen" @click="closeMenu" class="close-button">
         <span>&times;</span>
