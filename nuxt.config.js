77f71f1fbfe29ca3749e519572c02bf6dd819cce
Georgi Karchev <goshoo429@gmail.com>
2023-12-04 19:52:05 +0200

    Nuxt config file update

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,14 +10,4 @@ export default {
       autoprefixer: {},
     },
   },
-  router: {
-    extendRoutes(routes) {
-      routes.push(
-        { name: 'index', path: '/', component: path.resolve(__dirname, 'pages/index.vue') },
-        { name: 'about', path: '/about', component: path.resolve(__dirname, 'pages/about.vue') },
-        { name: 'portfolio', path: '/portfolio', component: path.resolve(__dirname, 'pages/portfolio.vue') },
-        { name: 'contact', path: '/contact', component: path.resolve(__dirname, 'pages/contact.vue') }
-      );
-    },
-  },
 };
